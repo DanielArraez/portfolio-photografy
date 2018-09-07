04bc2aad28d0d8e21a718e02b83f2e95839a037f
DanielArraez <35591117+DanielArraez@users.noreply.github.com>
2018-09-08 01:30:20 +0200

    D.

--- script.js
+++ script.js
@@ -11,6 +11,17 @@ window.onclick = function(e) {
  }
 }
 
+//Auto resize textarea//
+var tx = document.getElementsByTagName('textarea');
+for (var i = 0; i < tx.length; i++) {
+  tx[i].setAttribute('style', 'height:' + (tx[i].scrollHeight) + 'px;overflow-y:hidden;');
+  tx[i].addEventListener("input", OnInput, false);
+}
+
+function OnInput() {
+  this.style.height = 'auto';
+  this.style.height = (this.scrollHeight) + 'px';
+}
 
 // progressive-image.js
 if (window.addEventListener && window.requestAnimationFrame && document.getElementsByClassName) window.addEventListener('load', function() {
